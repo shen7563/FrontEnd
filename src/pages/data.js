c38f8192e0d222fc65b990f83294958dbd03fb62
shen7563 <qudgns5142@gmail.com>
2024-11-04 06:38:46 +0000

    data.js 옮기기

---
+++ src/pages/data.js
@@ -0,0 +1,19 @@
+let data = [
+    {
+        id : 0,
+        title : "제목1",
+        content : "제목1의 요약내용"
+    },
+    {
+        id : 1,
+        title : "제목2",
+        content : "제목2의 내용"
+    },
+    {
+        id : 2,
+        title : "제목3",
+        content : "제목3의 내용"
+    }
+]
+
+export default data;
