f833bb93303f83d18e5c6851eb313265e0100e27
YangByeongHun <qudgns5142@gmail.com>
2024-11-04 15:21:39 +0900

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import './App.css';
-import {Navbar, Container, Nav} from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import data from './data.js';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from './pages/Detail.js';
 
 function App() {
+
+  let [news_data] = useState(data);
+  let navigate = useNavigate();
+
   return (
     <div className="App">
+
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">
+          <Navbar.Brand onClick={() => { navigate('/') }}
+            style={{ cursor: 'pointer' }}>
             <img
               src="/Main_image.jpg"
               width="50"
@@ -14,24 +24,42 @@ function App() {
               alt="logo"
             />
             뉴스 및 여론 요약 서비스
-            </Navbar.Brand>
+          </Navbar.Brand>
           <Nav className="ms-auto">
-            <Nav.Link href="#주제1">주제1</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/') }}>home</Nav.Link>
             <Nav.Link href="#주제2">주제2</Nav.Link>
             <Nav.Link href="#주제3">주제3</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className="container">
-        <div className='row'>
-          <div className='col-md-4'>1번주제</div>
-          <div className='col-md-4'>2번주제</div>
-          <div className='col-md-4'>3번주제</div>
-          <div className='col-md-4'>4번주제</div>
-        </div>
-      </div>
+
+      <Routes>
+        <Route path="/" element={<div>
+          <div className="container">
+            <div className='row'>
+              {
+                news_data.map((a, i) => {
+                  return (
+                    <Card news_data={news_data[i]}></Card>
+                  )
+                })
+              }
+            </div>
+          </div></div>} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
     </div>
   );
+}
+
+function Card(props) {
+  return (
+    <div className='col-md-4'>
+      <h4>{props.news_data.title}</h4>
+      <p>{props.news_data.content}</p>
+    </div>
+  )
 }
 
 export default App;
