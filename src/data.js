31c5cb9e331363b133e20f225d6603121b2ae147
shen7563 <qudgns5142@gmail.com>
2024-11-04 06:23:02 +0000

    Add react-router-dom

---
+++ src/data.js
@@ -0,0 +1,20 @@
+let data = [
+    {
+        id : 0,
+        title : "주제1",
+        content : "주제1의 내용"
+    },
+    {
+        id : 1,
+        title : "주제2",
+        content : "주제2의 내용"
+    },
+    {
+        id : 2,
+        title : "주제3",
+        content : "주제3의 내용"
+    }
+
+]
+
+export default data;
