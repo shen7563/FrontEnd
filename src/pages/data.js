e9ff20bb626b9216c9bdd77171c62c3fd15afba8
YangByeongHun <qudgns5142@gmail.com>
2024-11-05 13:43:00 +0900

    이미지 넣기

--- src/pages/data.js
+++ src/pages/data.js
@@ -2,17 +2,20 @@ let data = [
     {
         id : 0,
         title : "제목1",
-        content : "제목1의 요약내용"
+        content : "제목1의 내용",
+        image : "./logo192.png"
     },
     {
         id : 1,
         title : "제목2",
-        content : "제목2의 내용"
+        content : "제목2의 내용",
+        image : "./logo192.png"
     },
     {
         id : 2,
         title : "제목3",
-        content : "제목3의 내용"
+        content : "제목3의 내용",
+        image : "./logo192.png"
     }
 ]
 
