d553ff52d5f1ab2e6bcc87d0d5e19dea8e5a71ff
shen7563 <qudgns5142@gmail.com>
2024-11-05 04:04:49 +0000

    ...

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import data from './pages/data.js';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import Detail from './pages/Detail.js';
 import axios from 'axios';
-import Temp from './pages/temp.js';
 
 function App() {
 
