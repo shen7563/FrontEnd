02ef1916b47f4fd5d3f7ee37adb7091a55fe9597
shen7563 <qudgns5142@gmail.com>
2024-11-09 19:02:37 +0000

    임시 데이터로 페이지 분할 되는지 확인

--- src/App.js
+++ src/App.js
@@ -2,27 +2,37 @@ import { useState } from 'react';
 import './App.css';
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+import Pagination from 'react-bootstrap/Pagination';
 import data from './pages/data.js';
 import Detail from './pages/Detail.js';
-import { Routes, Route, Link, useNavigate, useParams } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 
 function App() {
-
   let [newsdata] = useState(data);
   let [selectedTopic, setSelectedTopic] = useState(null);
+  let [currentPage, setCurrentPage] = useState(1);
   let navigate = useNavigate();
 
   const handleTopicClick = (topic) => {
     setSelectedTopic(topic);
+    setCurrentPage(1);
   }
 
   const handleMainClick = () => {
     setSelectedTopic(null);
+    setCurrentPage(1);
   }
 
+  const itemsPerPage = 10;
   const filteredNewsData = selectedTopic ? newsdata.filter(item => item.topic === selectedTopic) : newsdata;
+  const totalPages = Math.ceil(filteredNewsData.length / itemsPerPage);
+  const currentData = filteredNewsData.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
 
   const topics = [...new Set(data.map(item => item.topic))];
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  }
 
   return (
     <div className="App">
@@ -44,12 +54,12 @@ function App() {
           <div>
             <Container>
               <Row xs={1} md={2} className="g-4">
-                {filteredNewsData.map((item, index) => (
+                {currentData.map((item, index) => (
                   <Col key={index}>
                     <Card>
                       <Card.Img variant="top" src={item.image} style={{ width: '100px', height: '100px', objectFit: 'cover', margin: '0 auto' }} />
                       <Card.Body>
-                        <Card.Title onClick={() => { navigate(`/detail/${index}`) }} style={{ cursor: 'pointer' }}>{item.title}</Card.Title>
+                        <Card.Title onClick={() => { navigate(`/detail/${item.id}`) }} style={{ cursor: 'pointer' }}>{item.title}</Card.Title>
                         <Card.Text>
                           {item.content}
                           <br></br>
@@ -60,6 +70,21 @@ function App() {
                   </Col>
                 ))}
               </Row>
+              <Row>
+                <Col className="d-flex justify-content-center">
+                  <Pagination>
+                    <Pagination.First onClick={() => handlePageChange(1)} disabled={currentPage === 1} />
+                    <Pagination.Prev onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} />
+                    {[...Array(totalPages)].map((_, pageIndex) => (
+                      <Pagination.Item key={pageIndex + 1} active={pageIndex + 1 === currentPage} onClick={() => handlePageChange(pageIndex + 1)}>
+                        {pageIndex + 1}
+                      </Pagination.Item>
+                    ))}
+                    <Pagination.Next onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages} />
+                    <Pagination.Last onClick={() => handlePageChange(totalPages)} disabled={currentPage === totalPages} />
+                  </Pagination>
+                </Col>
+              </Row>
             </Container>
           </div>
         } />
@@ -69,6 +94,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
--- src/pages/data.js
+++ src/pages/data.js
@@ -52,7 +52,62 @@ let data = [
         image : "/logo192.png",
         topic : "주제3",
         link: "https://www.naver.com"
+    },
+    {
+        id : 6,
+        title : "제목7",
+        content : "제목7의 내용",
+        opinion : "제목7의 의견",
+        image : "/logo192.png",
+        topic : "주제2",
+        link: "https://www.naver.com"
+    },
+    {
+        id : 7,
+        title : "제목8",
+        content : "제목8의 내용",
+        opinion : "제목8의 의견",
+        image : "/logo192.png",
+        topic : "주제4",
+        link: "https://www.naver.com"
+    },
+    {
+        id: 8,
+        title: "제목9",
+        content: "제목9의 내용",
+        opinion: "제목9의 의견",
+        image: "/logo192.png",
+        topic: "주제1",
+        link: "https://www.naver.com"
+    },
+    {
+        id: 9,
+        title: "제목10",
+        content: "제목10의 내용",
+        opinion: "제목10의 의견",
+        image: "/logo192.png",
+        topic: "주제2",
+        link: "https://www.naver.com"
+    },
+    {
+        id: 10,
+        title: "제목11",
+        content: "제목11의 내용",
+        opinion: "제목11의 의견",
+        image: "/logo192.png",
+        topic: "주제3",
+        link: "https://www.naver.com"
+    },
+    {
+        id: 11,
+        title: "제목12",
+        content: "제목12의 내용",
+        opinion: "제목12의 의견",
+        image: "/logo192.png",
+        topic: "주제4",
+        link: "https://www.naver.com"
     }
+    
 ]
 
 export default data;
